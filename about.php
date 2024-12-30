e0da5a0e21de9ef064a64c70a7ae7373b147ce35
Aliasgar10 <aliasgarrampurawala2@gmail.com>
2024-12-30 16:06:49 +0530

    a31

--- about.php
+++ about.php
@@ -59,82 +59,7 @@
     <!-- End Page Header -->
 
     <!-- Start About Section -->
-    <section class="cs_pt_135 cs_pt_lg_75 cs_pb_140 cs_pb_lg_80 position-relative">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-6 cs_mb_lg_55">
-            <div class="cs_experience cs_style_1 position-relative">
-              <div class="cs_experience_thumb">
-                <img src="assets/img/experience_img.jpeg" alt="Thumb" class="position-relative cs_zindex_3 cs_rounded_15">
-                <div class="cs_experience_shape"><img src="assets/img/experience_shape_1.png" alt="Shape" class="moving_x"></div>
-              </div>
-              <div class="cs_experience_box background-filled text-center bg-white cs_rounded_10 position-absolute bottom-0 end-0 cs_zindex_3 d-flex flex-column justify-content-center align-items-center" data-src="assets/img/experience_bg.jpeg">
-                <img src="assets/img/experience_icon.svg" alt="Icon" class="cs_mb_5">
-                <h3 class="text-white cs_fs_60 cs_fs_lg_46 fw-bold lh_1 mb-0 d-flex justify-content-between">
-                  <span data-count-to="40" class="odometer"></span>
-                  <span class="fw-light">+</span>
-                </h3>
-                <h2 class="cs_fs_18 fw-normal text-white m-0">Work Experience</h2>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-6">
-            <div class="cs_about cs_style_1">
-              <div class="cs_section_heading cs_style_1 d-flex align-items-center cs_mb_15">
-                <div class="cs_section_heading_in">
-                  <h3 class="cs_fs_20 cs_fs_lg_18 text-accent fw-normal cs_lh_base cs_mb_10 wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.2s">About Us</h3>
-                  <h2 class="cs_fs_48 cs_fs_lg_36 cs_mb_20">Get Ready for Success with <span class="text-accent">TBL</span> Your Path to Growth.</h2>
-                  <p class="m-0">TBL Industrial & Engineering Exports Pvt Ltd started off as Jainex Industrial as a 50-50 partnership between the Jainex group and LMG Group registered in Mumbai with operational headquarters in Kolkata. The major thrust was exports and imports. TBL was primary focused on hydraulic applications which led to its first tie up with Parker Hannifin, the world leader in Hydraulics, Pneumatics, Filtration products and Seals. The first order was received in March 2002 for export to Bangladesh.</p>
-                </div>
-              </div>
-              <!-- <div class="cs_progressbar cs_style_1 cs_mb_20">
-                <div class="cs_progressbar_heading d-flex justify-content-between align-items-center cs_mb_8">
-                  <h3 class="fw-medium m-0 cs_fs_18">Web development</h3>
-                  <p class="cs_fs_14 cs_lh_base m-0">90%</p>
-                </div>
-                <div class="cs_progress cs_rounded_8 overflow-hidden" data-progress="90">
-                  <div class="cs_progress_in bg-accent cs_rounded_8 h-100 wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.2s"></div>
-                </div>
-              </div>.cs_progressbar -->
-              <!-- <div class="row cs_mb_15">
-                <div class="col-lg-6">
-                  <div class="cs_about-icon-box position-relative cs_mb_25">
-                    <span class="position-absolute cs_height_20 cs_width_20 top-0 start-0 cs_mt_5 bg-accent text-white cs_fs_10 d-flex align-items-center justify-content-center cs_rounded_30"><i class="fa-solid fa-angles-right"></i></span>
-                    <h3 class="cs_fs_16 cs_pl_35 cs_mb_12 cs_lh_lg">Accounting and Bookkeeping</h3>
-                    <p class="m-0">Services related to financial record-keeping, bookkeeping.</p>
-                  </div>
-                </div>
-                <div class="col-lg-6">
-                  <div class="cs_about-icon-box position-relative cs_mb_25">
-                    <span class="position-absolute cs_height_20 cs_width_20 top-0 start-0 cs_mt_5 bg-accent text-white cs_fs_10 d-flex align-items-center justify-content-center cs_rounded_30"><i class="fa-solid fa-angles-right"></i></span>
-                    <h3 class="cs_fs_16 cs_pl_35 cs_mb_12 cs_lh_lg">Human Resources (HR) Consulting</h3>
-                    <p class="m-0">Assistance with HR-related tasks such as recruitment.</p>
-                  </div>
-                </div>
-              </div> -->
-              <div class="d-flex align-items-center cs_row_gap_20 cs_column_gap_30 cs_column_gap_lg_20 flex-wrap">
-                <a href="contact.html" class="cs_btn cs_style_1 cs_fs_16 cs_rounded_5 cs_pl_30 cs_pr_30 cs_pt_10 cs_pb_10 overflow-hidden"><span>Get A Quote</span></a>
-                <a href="https://www.youtube.com/embed/VStvZjykQ00" class="cs_video_open d-flex align-items-center">
-                  <span class="cs_player_btn cs_width_45 cs_height_45 rounded-circle d-flex align-items-center justify-content-center text-white bg-primary position-relative cs_pl_5">
-                    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M1.71401 16C1.61636 16 1.51868 15.9748 1.43054 15.9246C1.25251 15.8231 1.14258 15.6339 1.14258 15.4285V0.570579C1.14258 0.365193 1.25251 0.176009 1.43054 0.0744495C1.61022 -0.026561 1.82844 -0.0243301 2.00475 0.0783627L14.5762 7.50735C14.7503 7.6106 14.8569 7.79755 14.8569 7.99957C14.8569 8.20159 14.7503 8.38855 14.5762 8.49179L2.00475 15.9207C1.9149 15.9732 1.81443 16 1.71401 16ZM2.28544 1.57172V14.4274L13.1621 7.99957L2.28544 1.57172Z" fill="white"/>
-                    </svg>   
-                  </span>
-                  <span class="cs_ml_15">Watch the video</span>               
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- <div class="cs_section_shape-1 position-absolute semi_rotate">
-        <svg width="182" height="177" viewBox="0 0 182 177" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M94.4478 96.1115C106.99 105.211 120.6 111.159 132.748 112.86C132.823 112.871 132.894 112.876 132.967 112.886L150.387 27.5688C151.002 24.552 147.98 22.0838 145.148 23.2887L63.8438 57.8735C67.5196 70.1011 78.6592 84.6548 94.4478 96.1115Z" fill="#888888" fill-opacity="0.2"/>
-          <path d="M128.155 120.487C115.612 118.057 102.053 111.863 89.5833 102.815C72.3806 90.3334 60.5954 75.0018 56.1891 61.1295L14.8513 78.7143C11.6218 80.0876 11.7968 84.724 15.1212 85.8483L74.9037 106.09C78.4203 107.281 81.3895 109.706 83.2572 112.915L115.015 167.46C116.78 170.492 121.358 169.74 122.061 166.301L131.308 121.013C130.265 120.863 129.215 120.692 128.155 120.487Z" fill="#888888" fill-opacity="0.2"/>
-          <path d="M129.41 114.012C116.866 111.583 103.307 105.387 90.8381 96.3405C61.4995 75.0532 47.8833 45.4701 59.84 28.9897C71.7975 12.512 104.146 16.278 133.483 37.5651C143.082 44.5285 151.409 52.7615 157.564 61.3736C158.895 63.2352 158.464 65.8208 156.603 67.1511C154.742 68.4799 152.156 68.0495 150.827 66.1896C145.184 58.2942 137.505 50.7138 128.621 44.2672C103.894 26.326 75.466 21.5559 66.5447 33.8531C57.6235 46.1491 70.9766 71.6938 95.7041 89.6352C108.246 98.7348 121.848 104.686 134.005 106.383C145.234 107.955 153.677 105.705 157.78 100.049C162.059 94.1524 160.568 86.183 158.563 80.5345C157.799 78.3788 158.926 76.0123 161.081 75.2472C163.236 74.4823 165.604 75.6089 166.369 77.7657C170.142 88.3972 169.474 98.04 164.484 104.915C158.497 113.168 147.265 116.603 132.859 114.587C131.719 114.43 130.568 114.237 129.41 114.012Z" fill="#888888" fill-opacity="0.2"/>
-        </svg>          
-      </div> -->
-    </section>
+    <?php include("inc_files/home_about.php"); ?>
     <!-- End About Section -->
   
     <!-- Start Service Section -->
@@ -471,73 +396,7 @@
     <!-- End Testimonial Section -->
 
     <!-- Start Footer -->
-    <footer class="cs_footer background-filled text-white" data-src="assets/img/footer_bg.jpeg">
-      <div class="container">
-        <div class="cs_footer_top d-flex justify-content-between align-items-start align-items-md-center cs_pt_46 cs_pb_46 cs_column_gap_15 cs_row_gap_15 flex-md-row  flex-column">
-          <div class="cs_footer_contact_info">
-            <h3 class="text-accent fw-normal cs_mb_4 cs_fs_16 cs_lh_lg">Have Any Question?</h3>
-            <h2 class="text-white fw-medium m-0 cs_fs_22">+ 070 4531 9507 </h2>
-          </div>
-          <div class="cs_footer_logo wow zoomIn" data-wow-duration="0.8s" data-wow-delay="0.2s"><img src="assets/img/logo.svg" alt="Logo"></div>
-          <div class="cs_footer_contact_info">
-            <h3 class="text-accent fw-normal cs_mb_4 cs_fs_16 cs_lh_lg">Send Email</h3>
-            <h2 class="text-white fw-medium m-0 cs_fs_22"><a href="mailto:bizmax@laralink.com">bizmax@laralink.com</a></h2>
-          </div>
-        </div>
-        <div class="cs_footer_main cs_pt_30 cs_pb_30">
-          <div class="row">
-            <div class="col-lg-4">
-              <div class="cs_footer_item cs_pt_20 cs_pb_20">
-                <div class="cs_text_widget">
-                  <p>I've been using [business name] for the past year and I'm so glad I did. Their products and services are top-notch and their customer service is amazing. I would highly recommend them to anyone</p>
-                </div>
-                <div class="cs_social_btns d-flex flex-wrap cs_column_gap_15 cs_row_gap_15 cs_transition_5">
-                  <a href="#" class="d-flex align-items-center justify-content-center cs_height_35 cs_width_35 text-white rounded-circle"><i class="fa-brands fa-facebook-f"></i></a>
-                  <a href="#" class="d-flex align-items-center justify-content-center cs_height_35 cs_width_35 text-white rounded-circle"><i class="fa-brands fa-twitter"></i></a>
-                  <a href="#" class="d-flex align-items-center justify-content-center cs_height_35 cs_width_35 text-white rounded-circle"><i class="fa-brands fa-linkedin-in"></i></a>
-                  <a href="#" class="d-flex align-items-center justify-content-center cs_height_35 cs_width_35 text-white rounded-circle"><i class="fa-brands fa-dribbble"></i></a>
-                </div>
-              </div>
-            </div>
-            <div class="col-lg-4 offset-lg-1">
-              <div class="cs_footer_item cs_pt_20 cs_pb_20">
-                <h2 class="cs_widget_title text-white cs_fs_22 cs_mb_22">Explore</h2>
-                <ul class="cs_menu_widget text-uppercase">
-                  <li><a href="about.html">About</a></li>
-                  <li><a href="portfolio.html">Portfolio</a></li>
-                  <li><a href="service.html">Services</a></li>
-                  <li><a href="#">Support </a></li>
-                  <li><a href="pricing.html">Pricing Page</a></li>
-                  <li><a href="portfolio.html">New Projects</a></li>
-                  <li><a href="blog-list-with-sidebar.html">Blog</a></li>
-                  <li><a href="">Privacy Policy</a></li>
-                  <li><a href="contact.html">Contact</a></li>
-                </ul>
-              </div>
-            </div>
-            <div class="col-lg-3">
-              <div class="cs_footer_item cs_pt_20 cs_pb_20">
-                <div class="cs_newsletter cs_style_1">
-                  <h2 class="cs_newsletter_title text-white cs_fs_22 cs_mb_9">Newsletter</h2>
-                  <p class="cs_newsletter_subtitle cs_mb_26"> At vero eos et accusamus et iusto odio as part dignissimos ducimus qui blandit. </p>
-                  <form action="#" class="cs_newsletter_form position-relative">
-                    <input type="email" class="cs_newsletter_input text-white cs_fs_14 cs_rounded_5 border-0 w-100 cs_pt_10" placeholder="Enter your mail">
-                    <button class="cs_newsletter_btn cs_fs_14 cs_rounded_5 cs_transition_4 bg-accent position-absolute text-uppercase">
-                      <span>Go</span>
-                    </button>
-                  </form>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="cs_copyright text-center cs_fs_124 cs_lh_lg cs_pt_36 cs_pb_36">
-        <div class="container">
-          <p class="m-0">Copyright © 2023 <a href="index.html">bizmax</a>. All rights reserved.</p>
-        </div>
-      </div>
-    </footer>
+    <?php include("inc_files/footer.php"); ?>
     <!-- End Footer -->
     <!-- Script -->
     <script src="assets/js/plugins/jquery-3.6.0.min.js"></script>
