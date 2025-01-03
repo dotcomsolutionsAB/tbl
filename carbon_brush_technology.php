ac6dc2fa405d3f364b51f643548ad6773b0568ad
Aliasgar10 <aliasgarrampurawala2@gmail.com>
2025-01-03 14:15:21 +0530

    a122

---
+++ carbon_brush_technology.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+  <head>
+    <!-- Meta Tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Laralink">
+    <!-- Favicon Icon -->
+    <link rel="icon" href="assets/img/favicon.png">
+    <!-- Site Title -->
+    <title>Home - TBL Industrial & Engineering Exports Pvt. Ltd</title>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/plugins/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/fontawesome.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/slick.css">
+    <link rel="stylesheet" href="assets/css/plugins/animate.css">
+    <link rel="stylesheet" href="assets/css/plugins/swiper.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/odometer.css">
+    <link rel="stylesheet" href="assets/css/default.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+  <body>
+    <!-- <div class="cs_preloader cs_accent_color cs_white_bg">
+      <div class="cs_preloader bg-white d-flex justify-content-center align-items-center">
+        <div class="cs_preloader_in">
+          <img src="assets/img/favicon.png" alt="Logo">
+        </div>
+      </div>
+    </div> -->
+    
+      <!-- Start Header Section -->
+    <?php include("inc_files/header2.php"); ?>
+      <!-- End Header Section -->
+
+    <!-- Start Page Header -->
+    <section class="cs_page_header position-relative background-filled d-flex align-items-center justify-content-between" data-src="assets/img/page_header_1.jpg">
+      <div class="container position-relative z-index-1">
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb text-white cs_fs_18 cs_mb_5">
+            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+            <li class="breadcrumb-item active">Carbon Brush Technology</li>
+          </ol>
+        </nav>
+        <h1 class="cs_fs_48 cs_fs_lg_36 text-white m-0">Carbon Brush Technology</h1>
+      </div>
+      <div class="position-absolute end-0 bottom-0">
+        <svg width="660" height="497" viewBox="0 0 660 497" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M240 0H660L430 497H0L240 0Z" fill="url(#paint0_linear_81_9510)"/>
+          <defs>
+          <linearGradient id="paint0_linear_81_9510" x1="330" y1="78.2497" x2="375.052" y2="780.743" gradientUnits="userSpaceOnUse">
+          <stop stop-color="white" stop-opacity="0" offset="none"/>
+          <stop offset="0.9999" stop-color="#D9D9D9" stop-opacity="0.35"/>
+          <stop offset="1" stop-color="#222121" stop-opacity="0"/>
+          <stop offset="1" stop-color="#222121" stop-opacity="0"/>
+          </linearGradient>
+          </defs>
+        </svg> 
+      </div>       
+    </section>
+    <!-- End Page Header -->
+
+    <!-- Start Service Section -->
+    <?php include("inc_files/service_2.php"); ?>
+    <!-- End Service Section -->
+
+    <!-- Start Footer -->
+    <?php include("inc_files/footer.php"); ?>
+    <!-- End Footer -->
+    <!-- Script -->
+    <script src="assets/js/plugins/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/plugins/isotope.pkg.min.js"></script>
+    <script src="assets/js/plugins/jquery.slick.min.js"></script>
+    <script src="assets/js/plugins/odometer.js"></script>
+    <script src="assets/js/plugins/ripples.min.js"></script>
+    <script src="assets/js/plugins/wow.min.js"></script>
+    <script src="assets/js/plugins/swiper.min.js"></script>
+    <script src="assets/js/plugins/YTPlayer.min.js"></script>
+    <script src="assets/js/main.js"></script>
+  </body>
+</html>
