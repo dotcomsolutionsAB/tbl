39a51f030c9c2149de5156d87c31073ce16095dd
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-01-10 14:20:50 +0530

    s153

--- admin/super/index.php
+++ admin/super/index.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="table_cdn/datatables.min.css">
+    <link rel="stylesheet" href="table_cdn/datatables.css">
     <title>Admin Dashboard</title>
 </head>
 
@@ -180,6 +181,7 @@
     
     <!-- for table -->
     <script src="table_cdn/datatables.min.js"></script>
+    <script src="table_cdn/datatables.js"></script>
 </body>
 
 </html>
