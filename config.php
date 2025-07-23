9524a3ad681b2c3c4bb9beb3dafe643157ae3df4
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-07-23 17:32:51 +0530

    ..

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php 
 // Live Crediential
-    // $host = 'localhost';
+    // $host = '127.0.0.1';
     // $dbname = 'tbl_';
     // $username = 'tbl_';
     // $password = '5bs&4z5U5';
 
 // Local Crediential
-    $host = 'localhost';
+    $host = '127.0.0.1';
     $dbname = 'tbl_';
     $username = 'root';
     $password = '';
